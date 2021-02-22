295e6b0b1af67263dfbbbed86e6d1626be37db83
kzresume <inz.zawisza.karol@gmail.com>
2021-02-22 17:30:46 +0100

    Fix and add a form

--- app_time/views.py
+++ app_time/views.py
@@ -5,8 +5,11 @@ import pytz
 
 
 def time_wold(request):
+    value = False
+    if request.GET.get('text'):
+        value = request.GET.get('text') 
     now = timezone.now()
-    context = {'now':now,'time_zones':pytz.all_timezones}
+    context = {'now':now, 'time_zones':pytz.all_timezones,'value':value}
     template = 'time_world.html'
     return render(request,template,context)
 
